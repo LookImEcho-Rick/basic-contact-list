a3dbd8ce8fbd89c56cfe1674c6c58ac25ca2cc49
LookImEcho-Rick <echosonlinestuff@gmail.com>
2025-09-07 00:13:12 -0500

    Fixed UI output

--- Basic Contact List.py
+++ Basic Contact List.py
@@ -13,9 +13,9 @@ contacts = [
 # This loops the program with a menu display that allows the user to choose an option
 while True:
     print("""---Contact List Menu---
-          1. Add a New Contact
-          2. View all Contacts
-          3. Quit""")
+1. Add a New Contact
+2. View all Contacts
+3. Quit""")
     choice = input("Select Your Choice: ")
     
     # If the user chooses '1', we ask for the new contact's details.
